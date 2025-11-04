b7ba51b66658f152b35941301cf769bdcee22319
Kim Seong Min <bomulsm@tukorea.ac.kr>
2025-11-04 23:53:47 +0900

    기존 공격1 f키로 설정하고 공격2 행동 추가(g키로 행동)

--- main.py
+++ main.py
@@ -15,25 +15,28 @@ class Character:
         self.idle_image = load_image('Fighter/Idle.png')
         self.walk_image = load_image('Fighter/Walk.png')
         self.attack_image = load_image('Fighter/Attack_1.png')
+        self.attack2_image = load_image('Fighter/Attack_3.png')
         self.frame = 0
 
         # 이미지 프레임 크기 계산
         self.idle_frame_width = self.idle_image.w // 6
         self.walk_frame_width = self.walk_image.w // 8
         self.attack_frame_width = self.attack_image.w // 4
+        self.attack2_frame_width = self.attack2_image.w // 4
         self.frame_height = self.walk_image.h
 
         # 행동 상태 초기화
         self.moving_left = False
         self.moving_right = False
         self.attacking = False
+        self.attacking2 = False
 
         # 프레임 타이머 초기화
         self.frame_time = 0
 
     def update(self):
         # 좌우 이동(공격 중이 아닐 때만)
-        if not self.attacking:
+        if not self.attacking and not self.attacking2:
             if self.moving_left:
                 self.x -= self.speed
             if self.moving_right:
@@ -55,6 +58,13 @@ class Character:
                 if self.frame == 4:
                     self.frame = 0
                     self.attacking = False
+        elif self.attacking2:
+            if self.frame_time >= 5:
+                self.frame+=1
+                self.frame_time = 0
+                if self.frame == 4:
+                    self.frame = 0
+                    self.attacking2 = False
         elif self.moving_left or self.moving_right:
             if self.frame_time >= 8:
                 self.frame = (self.frame + 1) % 8
@@ -70,9 +80,17 @@ class Character:
             self.frame = 0
             self.frame_time = 0
 
+    def attack2(self):
+        if not self.attacking2:
+            self.attacking2 = True
+            self.frame_time = 0
+            self.frame = 0
+
     def draw(self):
         if self.attacking:
             self.attack_image.clip_draw(self.frame * self.attack_frame_width, 0, self.attack_frame_width, self.frame_height, self.x, self.y, 200, 200)
+        elif self.attacking2:
+            self.attack2_image.clip_draw(self.frame * self.attack2_frame_width, 0, self.attack2_frame_width, self.frame_height, self.x, self.y, 200, 200)
         elif self.moving_left or self.moving_right:
             self.walk_image.clip_draw(self.frame * self.walk_frame_width, 0, self.walk_frame_width, self.frame_height, self.x, self.y, 200, 200)
         else:
@@ -92,8 +110,10 @@ def handle_events():
                 character.moving_left = True
             elif event.key == SDLK_d:
                 character.moving_right = True
-            elif event.key == SDLK_f or event.key == SDLK_g:
+            elif event.key == SDLK_f:
                 character.attack()
+            elif event.key == SDLK_g:
+                character.attack2()
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_a:
                 character.moving_left = False
