8a4e2dd86119502fa7f47a196e42fb74a8feae16
Kim Seong Min <bomulsm@tukorea.ac.kr>
2025-11-19 00:13:22 +0900

    메뉴 선택 화면부터 출력되도록 시작화면 설정

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
+from menu_manager import MenuManager
 from game_manager import GameManager
 
-# 게임 매니저 생성
-game = GameManager(width=1200, height=800)
 
-# 게임 초기화
-game.init()
+def main():
+    # 메뉴 시스템 시작
+    menu = MenuManager(width=1200, height=800)
+    menu.init()
 
-# 게임 실행
-game.run()
+    # 메뉴 루프
+    while True:
+        result = menu.handle_events()
 
-# 게임 종료
-game.close()
+        if result == 'quit':
+            menu.close()
+            break
+
+        elif result == 'start_game':
+            # 선택 사항 가져오기
+            selections = menu.get_selections()
+
+            # 메뉴 종료
+            menu.close()
+
+            # 게임 시작
+            game = GameManager(width=1200, height=800)
+            game.init(
+                character1_name=selections['player1_character'],
+                character2_name=selections['player2_character'],
+                enable_ai=selections['enable_ai'],
+                ai_difficulty=selections['ai_difficulty']
+            )
+
+            game.run()
+            game.close()
+
+            # 게임 종료 후 메뉴로 돌아가기
+            menu = MenuManager(width=1200, height=800)
+            menu.init()
+
+        menu.draw()
+
+
+if __name__ == '__main__':
+    main()
